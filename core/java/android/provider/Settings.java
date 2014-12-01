f038629103b7d68244b217e91315066f03f226ec
Jon Evans <jon@craftyjon.com>
2014-11-30 20:55:42 -0500

    PowerManagerService: Wake on plug (1/2)
    Change-Id: I5e50a7fd351857d76c1c2fd13c6e737d8902a2f3
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -21348,6 +21348,13 @@ public final class Settings {
                 "watch_ranging_available";
 
         /**
+         * Whether to wake the display when plugging or unplugging the charger
+         * @hide
+         */
+        public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED =
+                "wake_when_plugged_or_unplugged";
+
+        /**
          * Indicates if scene container flag is enabled. This is only a temporary addition that will
          * be removed for Android 17.1 release.
          *
