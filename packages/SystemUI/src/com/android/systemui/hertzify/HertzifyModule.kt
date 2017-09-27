4863989c6bb0598e9e1e6408180ad189a8b203d6
Roman Birg <roman@cyngn.com>
2017-09-27 21:01:50 +0200

    SystemUI: Add Compass tile
    neobuddy89:
    
    * Set default state to inactive
    * Remove slashed icon
    * Add check if device supports compass
    * Disable long click for Compass tile
    * Code clean up and adapt to Android 13, 14
    * Combine compass check in tile itself
    
    Squashed:
    
        From: timjosten <timjosten@gmail.com>
        Date: Thu, 18 Aug 2022 18:10:47 +0000
        Subject: SystemUI: Fix QS compass tile spam & instability
    
        E ViewRootImpl: Accessibility content change on non-UI thread. Future Android versions will throw an exception.
        E ViewRootImpl: android.view.ViewRootImpl$CalledFromWrongThreadException: Only the original thread that created a view hierarchy can touch its views.
        E ViewRootImpl:  at android.view.ViewRootImpl$SendWindowContentChangedAccessibilityEvent.runOrPost(ViewRootImpl.java:10375)
        E ViewRootImpl:  at android.view.ViewRootImpl.postSendWindowContentChangedCallback(ViewRootImpl.java:9203)
        E ViewRootImpl:  at android.view.ViewRootImpl.notifySubtreeAccessibilityStateChanged(ViewRootImpl.java:9383)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.View.notifySubtreeAccessibilityStateChangedIfNeeded(View.java:14002)
        E ViewRootImpl:  at android.view.View.setRotation(View.java:17015)
    
    Change-Id: Ib9b1ddded5f4cb5aa9535104473624162aadb28b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- packages/SystemUI/src/com/android/systemui/hertzify/HertzifyModule.kt
+++ packages/SystemUI/src/com/android/systemui/hertzify/HertzifyModule.kt
@@ -31,6 +31,7 @@ import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.LocaleTile
 import com.android.systemui.qs.tiles.PreferredNetworkTile
+import com.android.systemui.qs.tiles.CompassTile
 import com.android.systemui.qs.tiles.base.shared.model.QSTileConfig
 import com.android.systemui.qs.tiles.base.shared.model.QSTileUIConfig
 import com.android.systemui.res.R
@@ -109,6 +110,12 @@ interface HertzifyModule {
     @StringKey(PreferredNetworkTile.TILE_SPEC)
     fun bindPreferredNetworkTile(preferredNetworkTile: PreferredNetworkTile): QSTileImpl<*>
 
+    /** Inject CompassTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CompassTile.TILE_SPEC)
+    fun bindCompassTile(compassTile: CompassTile): QSTileImpl<*>
+
     companion object {
         const val AMBIENT_DISPLAY_TILE_SPEC = "ambient_display"
         const val AOD_TILE_SPEC = "aod"
@@ -121,6 +128,7 @@ interface HertzifyModule {
         const val DATA_SWITCH_TILE_SPEC = "dataswitch"
         const val LOCALE_TILE_SPEC = "locale"
         const val PREFERRED_NETWORK_TILE_SPEC = "preferred_network"
+        const val COMPASS_TILE_SPEC = "compass"
 
         @Provides
         @IntoMap
@@ -285,6 +293,21 @@ interface HertzifyModule {
                     ),
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.CONNECTIVITY,
+            )
+
+        @Provides
+        @IntoMap
+        @StringKey(COMPASS_TILE_SPEC)
+        fun provideCompassTile(uiEventLogger: QsEventLogger): QSTileConfig =
+            QSTileConfig(
+                tileSpec = TileSpec.create(COMPASS_TILE_SPEC),
+                uiConfig =
+                    QSTileUIConfig.Resource(
+                        iconRes = R.drawable.ic_qs_compass,
+                        labelRes = R.string.quick_settings_compass_label
+                    ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.UTILITIES,
             )
     }
 }
