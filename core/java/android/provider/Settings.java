e454a48b63c22bc34b5c241d286f5447d825b2a4
varund7726 <date.varun123@gmail.com>
2019-07-11 11:04:15 +0300

    SystemUI: Option to disable Data Disabled Indicator icon
    [someone5678] Adapt to SystemUI mobile connections pipeline
    [HELLBOY017] Move data disabled icon switch to statusbar tuner
    
    Change-Id: I4e5b058d9a19070494659f52be8d95892b0eaf5d
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6926,6 +6926,12 @@ public final class Settings {
         public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
         /**
+         * Whether to display cross sign for a data disabled connection
+         * @hide
+         */
+        public static final String DATA_DISABLED_ICON = "data_disabled_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
