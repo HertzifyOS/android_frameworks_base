789a2116eb695ef3f1edc20782acb786591306ed
althafvly <althafvly@gmail.com>
2020-05-26 21:17:59 +0800

    SystemUI: Support black theme for dark mode [1/3]
    Co-authored-by: Jesse Chan <jc@lineageos.org>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I57cd53de8f2c1e4d445441b514875b6af915b858
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14631,6 +14631,12 @@ public final class Settings {
          */
         public static final String VPN_ENFORCE_DNS_STORE = "vpn_enforce_dns_store";
 
+	/**
+         * Whether to use black theme for dark mode
+         * @hide
+         */
+        public static final String BERRY_BLACK_THEME = "berry_black_theme";
+        
         /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
