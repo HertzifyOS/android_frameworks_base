bea145f99c0996e529a02b1b2033ceed3046a990
althafvly <althafvly@gmail.com>
2020-05-26 21:17:59 +0800

    SystemUI: Support black theme for dark mode [1/3]
    Co-authored-by: Jesse Chan <jc@lineageos.org>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I57cd53de8f2c1e4d445441b514875b6af915b858
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13747,6 +13747,12 @@ public final class Settings {
         public static final String ENABLE_PROJECTION_PRIVACY_INDICATOR = "enable_projection_privacy_indicator";
 
         /**
+         * Whether to use black theme for dark mode
+         * @hide
+         */
+        public static final String BERRY_BLACK_THEME = "berry_black_theme";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
