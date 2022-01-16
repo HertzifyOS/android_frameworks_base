606bc4c2df974739c6d7aefb004239d3b5b06561
ezio84 <brabus84@gmail.com>
2022-01-16 13:20:36 +0400

    Allow to suppress notifications sound/vibration if screen is ON [1/2]
    Squashed:
    
        From: someone5678 <someone5678@users.noreply.github.com>
        Date: Sun, 30 Jun 2024 18:35:59 +0900
        Subject: fixup! Allow to suppress notifications sound/vibration if screen is ON [1/2]
    
        * Also apply it to NotificationAttentionHelper
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Tue, 23 Jul 2024 01:12:58 +0530
        Subject: fixup! Allow to suppress notifications
         sound/vibration if screen is ON [1/2]
    
        * We forgot to load settings on boot.
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I279b202682939d797d3116089f50d65e3dd3eb01
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7269,6 +7269,14 @@ public final class Settings {
         public static final String ENABLE_ROTATION_BUTTON = "enable_rotation_button";
 
         /**
+         * Whether to play notification sound and vibration if screen is ON
+         * 0 - never
+         * 1 - always
+         * @hide
+         */
+        public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
