c3c681268b34ef543a737e4fa58e069090ce0fbf
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-24 00:24:27 +0530

    SystemUI: Allow disabling clipboard overlay [1/2]
    Change-Id: Ia755ca9021468a53c8f17413ca3faa787b00ceea
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14468,6 +14468,13 @@ public final class Settings {
         public static final String SPOOF_APPPROPS_CONFIG = "spoof_appprops_config";
 
         /**
+         * Whether to show an overlay in the bottom corner of the screen on copying stuff
+         * into the clipboard.
+         * @hide
+         */
+        public static final String SHOW_CLIPBOARD_OVERLAY = "show_clipboard_overlay";
+        
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
