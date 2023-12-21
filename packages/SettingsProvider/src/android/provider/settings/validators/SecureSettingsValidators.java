4350186238282655b42ca3d1173e10ea6c27b0a5
Ido Ben-Hur <idoybh2@gmail.com>
2023-12-21 16:02:57 +0200

    base: Allow disabling private DNS for VPN [1/2]
    Allows using the VPN's DNS instead of the set private DNS
    Automatically restores the previous set mode
    
    Change-Id: I48cdb0f50489210c7b5f0abebfeab35094040fa3
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -555,5 +555,7 @@ public class SecureSettingsValidators {
                 COLON_SEPARATED_COMPONENT_LIST_VALIDATOR);
         VALIDATORS.put(Secure.DOZE_TAP_GESTURE_AMBIENT, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.DOZE_PICK_UP_GESTURE_AMBIENT, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(Secure.VPN_ENFORCE_DNS, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(Secure.VPN_ENFORCE_DNS_STORE, new DiscreteValueValidator(new String[] {"-1", "0", "1", "2"}));
     }
 }
