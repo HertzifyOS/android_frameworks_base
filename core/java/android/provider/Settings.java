55e5c448fc02232208894a50c49eeb523b7b1829
Pranav Vashi <neobuddy89@gmail.com>
2024-03-24 23:59:24 +0530

    SystemUI: Allow toggling rotation button suggestion [1/2]
    * Move logic to RotationButtonController rather defining in NavigationBarView.
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: I870ce1dd54a97d85c418e5c4f1d00023871a2ec5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7032,6 +7032,13 @@ public final class Settings {
         @Readable
         public static final String ADVANCED_REBOOT = "advanced_reboot";
 
+       /**
+         * Whether to show rotation suggestion
+         * @hide
+         */
+        @Readable
+        public static final String ENABLE_ROTATION_BUTTON = "enable_rotation_button";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
