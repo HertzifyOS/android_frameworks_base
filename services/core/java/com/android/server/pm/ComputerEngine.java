ccf75aa3210a62906aada72a07bad9b931f98297
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-03 18:29:50 +0800

    Hide App list: Improve hide app list feature
    Insprired by: https://github.com/j-hc/zygisk-detach, instead of hooking libbinder, we override the computerengine to hide the app to the caller package. we should hide apps whereever possible except for system apps like launcher,settings, and framework. The main goal of Hide app list is to hide apps from playstore and banking apps, however we don't want to break package installer and system app list.
    
    Change-Id: I6d6cb159bde5dbabf37f313363996943ac881623
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -185,6 +185,8 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
+import com.android.internal.util.hertzify.HideAppListUtils;
+
 /**
  * This class contains the implementation of the Computer functions.  It
  * is entirely self-contained - it has no implicit access to
@@ -997,6 +999,10 @@ public class ComputerEngine implements Computer {
 
     public final ApplicationInfo getApplicationInfo(String packageName,
             @PackageManager.ApplicationInfoFlagsBits long flags, int userId) {
+        if (canHideApp(Binder.getCallingUid(), packageName) &&
+            HideAppListUtils.shouldHideAppList(mContext, packageName)) {
+            return null;
+        }
         return getApplicationInfoInternal(packageName, flags, Binder.getCallingUid(), userId);
     }
 
@@ -1010,6 +1016,10 @@ public class ComputerEngine implements Computer {
             @PackageManager.ApplicationInfoFlagsBits long flags,
             int filterCallingUid, int userId) {
         if (!mUserManager.exists(userId)) return null;
+        if (canHideApp(Binder.getCallingUid(), packageName) &&
+            HideAppListUtils.shouldHideAppList(mContext, packageName)) {
+            return null;
+        }
         flags = updateFlagsForApplication(flags, userId);
 
         if (!isRecentsAccessingChildProfiles(Binder.getCallingUid(), userId)) {
@@ -1019,6 +1029,64 @@ public class ComputerEngine implements Computer {
         }
 
         return getApplicationInfoInternalBody(packageName, flags, filterCallingUid, userId);
+    }
+    
+     private boolean canHideApp(int callingUid, String packageName) {
+        if (!isBootCompleted() || mContext == null || mContext.getPackageManager() == null) {
+            return false;
+        }
+
+        String callingPackage = mContext.getPackageManager().getNameForUid(callingUid);
+
+        if (callingPackage == null || TextUtils.isEmpty(callingPackage)) {
+            return false;
+        }
+
+        // app can be always hidden if calling package is play store
+        boolean isFinsky = callingPackage.contains("com.android.vending");
+
+        if (isFinsky) return true;
+        
+        if (packageName == null || TextUtils.isEmpty(packageName)) {
+            return false;
+        }
+        
+        // the calling package is itself, no need to hide
+        if (callingPackage.contains(packageName)) return false;
+
+        // we only want to hide these apps from playstore 
+        // to avoid these apps from being updated, so abort if
+        // calling package is not finsky
+        if (packageName.contains("youtube") 
+            || packageName.contains("microg")
+            || packageName.contains("revanced")
+            || packageName.contains("gms")) {
+            return false;
+        }
+
+        // this is for banking apps, but we need to make sure first that 
+        // we arent hiding app infos from sandbox/system processes
+        return !isCallerSystem(callingUid) 
+            && !Process.isIsolated(callingUid)
+            && !Process.isSdkSandboxUid(callingUid);
+    }
+    
+    public ParceledListSlice<PackageInfo> recreatePackageList(
+            int callingUid, Context context, int userId, ParceledListSlice<PackageInfo> list) {
+        List<PackageInfo> appList = new ArrayList<>(list.getList());
+        if (!canHideApp(callingUid, null)) return new ParceledListSlice<>(appList);
+        Set<String> hiddenApps = HideAppListUtils.getApps(context);
+        appList.removeIf(info -> hiddenApps.contains(info.packageName));
+        return new ParceledListSlice<>(appList);
+    }
+
+    public List<ApplicationInfo> recreateApplicationList(
+            int callingUid, Context context, int userId, List<ApplicationInfo> list) {
+        List<ApplicationInfo> appList = new ArrayList<>(list);
+        if (!canHideApp(callingUid, null)) return appList;
+        Set<String> hiddenApps = HideAppListUtils.getApps(context);
+        appList.removeIf(info -> hiddenApps.contains(info.packageName));
+        return appList;
     }
 
     protected ApplicationInfo getApplicationInfoInternalBody(String packageName,
@@ -1700,6 +1768,10 @@ public class ComputerEngine implements Computer {
 
     public final PackageInfo getPackageInfo(String packageName,
             @PackageManager.PackageInfoFlagsBits long flags, int userId) {
+        if (canHideApp(Binder.getCallingUid(), packageName) &&
+            HideAppListUtils.shouldHideAppList(mContext, packageName)) {
+            return null;
+        }
         return getPackageInfoInternal(packageName, PackageManager.VERSION_CODE_HIGHEST,
                 flags, Binder.getCallingUid(), userId);
     }
@@ -1826,7 +1898,8 @@ public class ComputerEngine implements Computer {
         Slog.i(TAG, "getInstalledPackages: callingUid=" + callingUid + " flags=" + flags
                + " updatedFlags=" + updatedFlags + " userId=" + userId);
 
-        return getInstalledPackagesBody(updatedFlags, userId, callingUid);
+        return recreatePackageList(callingUid, mContext,
+                        userId, getInstalledPackagesBody(updatedFlags, userId, callingUid));
     }
 
     private PackageInfoList getInstalledPackagesBody(long flags, int userId, int callingUid) {
@@ -2699,7 +2772,7 @@ public class ComputerEngine implements Computer {
             return false;
         }
         // if the target is included in Settings.Secure.HIDE_APPLIST, do filter
-        if (com.android.internal.util.hertzify.HideAppListUtils.shouldHideAppList(
+        if (canHideApp(Binder.getCallingUid(), packageName) && HideAppListUtils.shouldHideAppList(
                 mContext, packageName)) {
             return true;
         }
@@ -4935,7 +5008,7 @@ public class ComputerEngine implements Computer {
             }
         }
 
-        return list;
+        return recreateApplicationList(callingUid, mContext, userId, list);
     }
 
     @Nullable
