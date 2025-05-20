00aee3c1b8d5f24d8a8c09941f1ba4f161cafe3f
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-20 08:45:49 +0800

    inputmethod: allow hiding navigation ime bar [1/3]
    Change-Id: Iaa15dda08d614d01fae64ae2b5aa72f50513af99
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7256,6 +7256,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_HINT = "navigation_bar_hint";
 
         /**
+         * boolean value. Whether to hide the navigation bar when the IME is shown.
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_IME = "navigation_bar_ime";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
