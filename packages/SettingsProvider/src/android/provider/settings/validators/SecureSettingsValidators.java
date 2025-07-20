70a08709e12e1f71d2c94b82024d11a84e016965
Abhay Singh Gill <abhaygill017@gmail.com>
2025-07-20 22:30:41 +0530

    SystemUI: Allow to change brightness slider positioning in new compose QS
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I57a55c924be7e36d969af4382400e8f0d9415a84
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -528,5 +528,9 @@ public class SecureSettingsValidators {
         VALIDATORS.put(Secure.FEATURE_TOUCH_HOVERING, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.VPN_ENFORCE_DNS, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.VPN_ENFORCE_DNS_STORE, new DiscreteValueValidator(new String[] {"-1", "0", "1", "2"}));
+        VALIDATORS.put(Secure.QS_SHOW_BRIGHTNESS_SLIDER,
+                new InclusiveIntegerRangeValidator(0, 2));
+        VALIDATORS.put(Secure.QS_BRIGHTNESS_SLIDER_POSITION,
+                new InclusiveIntegerRangeValidator(0, 1));
     }
 }
