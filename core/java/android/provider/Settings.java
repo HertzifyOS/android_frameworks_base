8daf4e703234f25994107f3f320a4a67bf55e742
Pranav Vashi <neobuddy89@gmail.com>
2025-12-14 23:21:46 +0530

    SystemUI: Add customization to show battery percent
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: Ic14d30987eac9b412684db987698e224b93a8071
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7284,6 +7284,15 @@ public final class Settings {
         public static final String STATUS_BAR_CLOCK_AUTO_HIDE = "status_bar_clock_auto_hide";
 
         /**
+         * Status bar battery %
+         * 0: Hide the battery percentage
+         * 1: Display the battery percentage inside the icon
+         * 2: Display the battery percentage next to the icon
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
