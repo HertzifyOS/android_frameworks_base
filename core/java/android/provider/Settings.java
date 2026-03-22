ea05391fa91b1c100ff3189015e7b5d64456d886
grepfox <grepfox@tutamail.com>
2026-03-22 11:22:41 +0530

    base: Allow hiding statusbar while capturing screenshots [1/2]
    Change-Id: If20ff2fb6b24f0b4cc2868f5fe659ca97e7081ec
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7288,6 +7288,11 @@ public final class Settings {
         public static final String SCREENSHOT_CLIPBOARD_ONLY = "screenshot_clipboard_only";
    
         /**
+         * @hide
+         */
+        public static final String HIDE_STATUS_BAR_IN_SCREENSHOT = "hide_status_bar_in_screenshot";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
