6301244f5d0d432e8f25e324eefaff8665cfceba
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-04-26 18:43:58 +0800

    core: Fixing spoofing issues
    Change-Id: Ie444b3f36d320e3b56b7e757a228516a147b84c5
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- core/java/android/security/appprops/AppPropsSpoofService.java
+++ core/java/android/security/appprops/AppPropsSpoofService.java
@@ -17,6 +17,7 @@
 package android.security.appprops;
 
 import android.app.ActivityManager;
+import android.app.IActivityManager;
 import android.os.Build;
 import android.os.RemoteException;
 import android.util.JsonReader;
@@ -63,10 +64,16 @@ public final class AppPropsSpoofService {
         mEnabled = false;
         mConfigLoaded = false;
 
+        IActivityManager am = ActivityManager.getService();
+        if (am == null) {
+            Log.w(TAG, "ActivityManager not ready, skipping gameprops config load");
+            return;
+        }
+
         String content;
         try {
-            content = ActivityManager.getService().getSpoofAppPropsConfig();
-        } catch (RemoteException e) {
+            content = am.getSpoofAppPropsConfig();
+        } catch (Throwable e) {
             Log.e(TAG, "Failed to fetch appprops config from system_server", e);
             return;
         }
