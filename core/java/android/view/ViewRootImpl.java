b2d436893f59b843cdbbdaa073ca95c193609070
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-05-02 11:16:35 +0800

    core: Caching views to optimize performance
    Change-Id: Ia4d67a5ba70bd66d9711c46494b3c7492b8c2eac
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -301,6 +301,7 @@ import com.android.internal.policy.DecorView;
 import com.android.internal.policy.PhoneFallbackEventHandler;
 import com.android.internal.protolog.ProtoLog;
 import com.android.internal.util.FastPrintWriter;
+import com.android.internal.util.ViewCacheManager;
 import com.android.internal.view.BaseSurfaceHolder;
 import com.android.internal.view.RootViewSurfaceTaker;
 import com.android.internal.view.SurfaceCallbackHelper;
@@ -695,6 +696,7 @@ public final class ViewRootImpl implements ViewParent,
     private boolean mDragResizing;
     private boolean mDragResizingCujBegun;
     private boolean mInvalidateRootRequested;
+    private boolean mFirstFrameDrawn = true;
     private int mCanvasOffsetX;
     private int mCanvasOffsetY;
     CompatibilityInfo.Translator mTranslator;
@@ -3395,6 +3397,10 @@ public final class ViewRootImpl implements ViewParent,
             }
         } else {
             mQueue.removeSyncBarrier(mTraversalBarrier);
+            if (mFirstFrameDrawn) {
+                ViewCacheManager.getInstance().onTraversalEnd(this);
+                mFirstFrameDrawn = false;
+            }
         }
     }
 
