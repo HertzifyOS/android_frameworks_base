7991143ccbfcd7348b5125bb61a433f3eba5b6b3
Ghosuto <clash.raja10@gmail.com>
2026-05-04 06:04:23 +0000

    SystemUI: Fix custom clock scale clipping
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
+++ packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
@@ -412,6 +412,22 @@ public class ClockStyle extends RelativeLayout implements TunerService.Tunable {
         return clamped / 100f;
     }
 
+    private void disableClippingOnParents(View view) {
+        setClipChildren(false);
+        setClipToPadding(false);
+
+        View current = view;
+        while (current != null && current != ClockStyle.this) {
+            if (current instanceof ViewGroup) {
+                ((ViewGroup) current).setClipChildren(false);
+                ((ViewGroup) current).setClipToPadding(false);
+            }
+            ViewParent parent = current.getParent();
+            if (!(parent instanceof View)) break;
+            current = (View) parent;
+        }
+    }
+
     private void applyClockScale() {
         if (currentClockView == null) return;
         float scale = getScaleFactor();
@@ -419,6 +435,7 @@ public class ClockStyle extends RelativeLayout implements TunerService.Tunable {
         currentClockView.setScaleY(scale);
         currentClockView.setPivotX(currentClockView.getWidth() / 2f);
         currentClockView.setPivotY(0f);
+        disableClippingOnParents(currentClockView);
     }
 
     private void animateAodTransition(boolean toAod) {
@@ -562,6 +579,7 @@ public class ClockStyle extends RelativeLayout implements TunerService.Tunable {
 
             if (currentClockView != null) {
                 preloadFonts(currentClockView);
+                disableClippingOnParents(currentClockView);
             }
             
             if (currentClockView != null) {
