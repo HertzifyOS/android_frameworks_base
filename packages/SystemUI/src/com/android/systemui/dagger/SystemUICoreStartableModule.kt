VoltageOS-staging/frameworks_base_new

fwb: battery stats notification
Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
@@ -53,6 +53,7 @@ import com.android.systemui.shortcut.ShortcutKeyDispatcher
 import com.android.systemui.statusbar.ImmersiveModeConfirmation
 import com.android.systemui.statusbar.gesture.GesturePointerEventListener
 import com.android.systemui.smartpixels.SmartPixelsReceiver
+import com.android.systemui.voltage.BatteryInfoNotificationController
 import com.android.systemui.statusbar.notification.InstantAppNotifier
 import com.android.systemui.statusbar.notification.headsup.StatusBarHeadsUpChangeListener
 import com.android.systemui.stylus.StylusUsiPowerStartable
@@ -335,5 +336,12 @@ abstract class SystemUICoreStartableModule {
     @ClassKey(SysUIKeyGestureEventInitializer::class)
     abstract fun bindSysUIKeyGestureEventInitializer(
         keyGestureEventInitializer: SysUIKeyGestureEventInitializer
+    ): CoreStartable
+
+    @Binds
+    @IntoMap
+    @ClassKey(BatteryInfoNotificationController::class)
+    abstract fun bindBatteryInfoNotificationController(
+        impl: BatteryInfoNotificationController
     ): CoreStartable
 }

HertzifyOS/android_frameworks_base

Change-Id: I57518afde526eadb19f1171c7f38191c46501329
Signed-off-by: HDzungx <hdzungx@gmail.com>

packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
@@ -52,6 +52,7 @@ import com.android.systemui.mediaprojection.taskswitcher.MediaProjectionTaskSwit
 import com.android.systemui.shortcut.ShortcutKeyDispatcher
 import com.android.systemui.statusbar.ImmersiveModeConfirmation
 import com.android.systemui.statusbar.gesture.GesturePointerEventListener
+import com.android.systemui.hertzify.BatteryInfoNotificationController
 import com.android.systemui.statusbar.notification.InstantAppNotifier
 import com.android.systemui.statusbar.notification.headsup.StatusBarHeadsUpChangeListener
 import com.android.systemui.stylus.StylusUsiPowerStartable
@@ -336,4 +337,11 @@ abstract class SystemUICoreStartableModule {
     @IntoMap
     @ClassKey(KeyguardSmartspaceStartable::class)
     abstract fun bindKeyguardSmartspaceStartable(impl: KeyguardSmartspaceStartable): CoreStartable
+
+    @Binds
+    @IntoMap
+    @ClassKey(BatteryInfoNotificationController::class)
+    abstract fun bindBatteryInfoNotificationController(
+        impl: BatteryInfoNotificationController
+    ): CoreStartable
 }